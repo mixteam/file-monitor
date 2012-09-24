09e5555e517d40f2a56393773238be04bf54cc04
terrykingcha <terrykingcha@gmail.com>
2012-09-24 18:32:22 +0800

    文件监控工具v0.1.0

---
+++ bin/monitor.js
@@ -0,0 +1,197 @@
+#!/usr/bin/env node
+// -*- js -*-
+
+var fs = require("fs"), 
+	path = require("path"),
+	util = require('util'),
+	spawn = require('child_process').spawn, 
+	exec = require('child_process').exec,
+
+	VERSION = '0.1.0',
+	INTERVAL = 500,
+
+	options = {
+		recursion : false,
+		monitor_file : '.monitor'
+	},
+	workingDir,
+
+	CMD_REGEXP = /^\[([^\[\]]+)\]$/,
+	EMPTY_LINE_REGEXP = /^[\r\n]*$/
+	;
+
+process.on('uncaughtException', function (err) {
+  console.error('Caught exception: ' + err);
+});
+
+function shouldMonitor(dirname) {
+	var file = path.join(dirname, options.monitor_file);
+
+	return fs.existsSync(file);
+}
+
+function parseMonitor(monitorFile) {
+	var lines = monitorFile.split(/[\r\n]/g),
+		monitorList = [],
+		monitorGroup
+		;
+
+	lines.forEach(function(line) {
+		var cmds;
+
+		if (line.match(EMPTY_LINE_REGEXP)) return;
+
+		if ((cmds = line.match(CMD_REGEXP))) {
+			if (monitorGroup) {
+				monitorList.push(monitorGroup);
+				monitorGroup = undefined;
+			}
+
+			monitorGroup = { commands : cmds[1].split(','), files : []}
+		} else {
+			monitorGroup.files.push(line);
+		}
+	});
+
+	if (monitorGroup) {
+		monitorList.push(monitorGroup);
+	}
+
+	return monitorList;
+}
+
+function runCmds(dirname, cmds) {
+	var opt = {
+		cwd : dirname, 
+		encoding : 'utf8'
+	}
+
+	cmds.forEach(function(cmd) {
+		exec(cmd, opt, function(error, stdout, stderr) {
+			util.log(stdout);
+			util.error(stderr);
+		});
+	});
+}
+
+function monitorFile(file, callback) {
+	var lastTimestamp = fs.statSync(file).mtime.getTime();
+
+	return setInterval(function() {
+		var stat = fs.statSync(file),
+			timestamp = stat.mtime.getTime()
+			;
+
+		if (timestamp !== lastTimestamp) {
+			lastTimestamp = timestamp;
+
+			callback();
+		}
+	}, INTERVAL);
+}
+
+function monitorGroup(dirname, group) {
+	var cmds = group.commands,
+		files = group.files,
+		fileMonitorId = []
+		;
+
+	files.forEach(function(file) {
+		var id;
+
+		filepath = path.join(dirname, file);
+
+		id = monitorFile(filepath, function() {
+			runCmds(dirname, cmds);
+		});
+
+		fileMonitorId.push(id);
+	});
+
+}
+
+function monitorDir(dirname) {
+	var filepath = path.join(dirname, options.monitor_file),
+		strFile = fs.readFileSync(filepath, 'utf8'),
+		monitorList = parseMonitor(strFile)
+		;
+
+	
+	monitorList.forEach(function(group) {
+		monitorGroup(dirname, group);
+	})
+	
+    util.debug(new Date().toTimeString().match(/\d{1,2}\:\d{1,2}\:\d{1,2}/g)[0] + 
+			' - [monitor] success to "' + 
+			dirname + '"\n');
+}
+
+function recurs(dirname, recursion) {
+	var dirlist = [],
+		indir
+
+
+	if (shouldMonitor(dirname)) {
+		dirlist.push(dirname);
+	}
+
+	if (recursion) {
+		indir = fs.readdirSync(dirname);
+		indir.forEach(function(name) {
+			if (!(name in ['.', '..', '.git', '.svn'])) {
+				var pathname = path.join(dirname, name),
+					stat = fs.statSync(pathname)
+					;
+
+				if (stat.isDirectory()) {
+					dirlist = dirlist.concat(recurs(pathname, recursion));
+				}
+			}
+		});
+	}
+
+	return dirlist;
+}
+
+function parse(dirname) {
+	var recursion = options.recursion,
+		dirlist
+		;
+
+	dirlist = recurs(dirname, recursion);
+
+	dirlist.forEach(function(dir) {
+		monitorDir(dir);
+	});
+}
+
+if (require.main === module) {
+
+	var argv = process.argv.slice(2)
+		;
+
+	while (argv.length > 0) {
+		var v = argv.shift();
+		switch(v) {
+			case '-r':
+			case '--recursion':
+				options.recursion = true;
+				break;
+			case '-m':
+			case '--monitor':
+				options.monitor_file = argv.shift();
+				break;
+			case '-v':
+			case '--version':
+				util.print('version ' + VERSION);
+				process.exit(0);
+			default:
+				workingDir = v;
+				break;
+		}
+	}
+
+	parse(workingDir);
+} else {
+	module.exports = monitor;
+}
